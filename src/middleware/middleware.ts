30069a477a83c715fd9013e4ac3e7b7243d5d687
Joni Vainio-Kaila <joni.vainio-kaila@vincit.fi>
2024-12-22 13:09:29 +0200

    refactor(middleware): Refactor route related logic to utility functions

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -2,22 +2,25 @@ import { match } from "@formatjs/intl-localematcher";
 import Negotiator from "negotiator";
 import { schema, type Locale } from "next-globe-gen/schema";
 import { NextResponse, type NextRequest } from "next/server";
-import { compile, match as pathMatcher } from "path-to-regexp";
+import { compile } from "path-to-regexp";
+import { extractLocaleAndRoutePathname, matchRoute } from "~/utils/routes";
 
 export function middleware(request: NextRequest) {
-  const [locale_, pathname] = getLocaleAndPathname(request.nextUrl.pathname);
+  const pathname = request.nextUrl.pathname;
+  const [locale_, routePathname] = extractLocaleAndRoutePathname(pathname);
+
   let locale = locale_;
 
   // Redirect to nonprefixed URL if default locale is not wanted
   if (!schema.prefixDefaultLocale && locale === schema.defaultLocale) {
-    return NextResponse.redirect(new URL(pathname, request.url));
+    return NextResponse.redirect(new URL(routePathname, request.url));
   }
 
   // Redirect to locale prefixed URL
   if (schema.prefixDefaultLocale && !locale) {
     const matchedLocale = localeMatcher(request);
     return NextResponse.redirect(
-      new URL(`/${matchedLocale}${pathname}`, request.url)
+      new URL(`/${matchedLocale}${routePathname}`, request.url)
     );
   }
 
@@ -26,26 +29,10 @@ export function middleware(request: NextRequest) {
 
   // Apply alternative localized links
   const response = NextResponse.next();
-  const alternativeLinks = getAlternativeLinks(locale, pathname);
+  const alternativeLinks = getAlternativeLinks(locale, request);
   if (!alternativeLinks) return response;
-
-  const linkHeader = alternativeLinks
-    .map(({ locale, alternate }) => {
-      const url = new URL(alternate, request.url);
-      url.search = "";
-      return `<${url}>; rel="alternate"; hreflang="${locale}"`;
-    })
-    .join(", ");
-  response.headers.set("Link", linkHeader);
-
+  response.headers.set("Link", alternativeLinks);
   return response;
-}
-
-function getLocaleAndPathname(pathname: string) {
-  const regexp = new RegExp(`\\/(${schema.locales.join("|")})(\\/?.*)`);
-  const match = pathname.match(regexp);
-  if (!match) return [undefined, pathname] as const;
-  return [match[1] as Locale, match[2] || "/"] as const;
 }
 
 function localeMatcher(request: NextRequest) {
@@ -54,17 +41,16 @@ function localeMatcher(request: NextRequest) {
   return match(negotiator.languages(), schema.locales, schema.defaultLocale);
 }
 
-function getAlternativeLinks(locale: Locale, pathname: string) {
-  let params: Partial<Record<string, string | string[]>>;
-  const localizedPaths = Object.values(schema.routes).find((localizedPaths) => {
-    const match = pathMatcher(localizedPaths[locale]!)(pathname);
-    if (!match) return false;
-    params = match.params;
-    return true;
-  });
-  if (!localizedPaths) return undefined;
-  return schema.locales.map((locale) => ({
-    locale,
-    alternate: compile(localizedPaths[locale]!)(params),
-  }));
+function getAlternativeLinks(locale: Locale, request: NextRequest) {
+  const routeMatch = matchRoute(locale, request.nextUrl.pathname);
+  if (!routeMatch) return undefined;
+  const { localizedPaths, params } = routeMatch;
+  return schema.locales
+    .map((locale) => {
+      const alternatePath = compile(localizedPaths[locale]!)(params);
+      const alternateURL = new URL(alternatePath, request.url);
+      alternateURL.search = "";
+      return `<${alternateURL}>; rel="alternate"; hreflang="${locale}"`;
+    })
+    .join(", ");
 }
---
+++ src/utils/routes.ts
@@ -0,0 +1,40 @@
+import {
+  schema,
+  type Locale,
+  type Route,
+  type RouteParams,
+  type Schema,
+} from "next-globe-gen/schema";
+
+import { match as pathMatcher } from "path-to-regexp";
+
+export function extractLocaleAndRoutePathname(pathname: string) {
+  const regexp = new RegExp(`\\/(${schema.locales.join("|")})(\\/?.*)`);
+  const match = pathname.match(regexp);
+  if (!match) return [undefined, pathname] as const;
+  return [match[1] as Locale, match[2] || "/"] as const;
+}
+
+type RouteMatch<R extends Route = Route> = {
+  route: R;
+  localizedPaths: Schema["routes"][R];
+  params: RouteParams<R>;
+};
+
+export function matchRoute(
+  locale: Locale,
+  pathname: string
+): RouteMatch | undefined {
+  let params: Partial<Record<string, string | string[]>> = {};
+  const routeMatch = Object.entries(schema.routes).find(
+    ([_, localizedPaths]) => {
+      const match = pathMatcher(localizedPaths[locale]!)(pathname);
+      if (!match) return false;
+      params = match.params;
+      return true;
+    }
+  );
+  if (!routeMatch) return undefined;
+  const [route, localizedPaths] = routeMatch;
+  return { route, localizedPaths, params };
+}
