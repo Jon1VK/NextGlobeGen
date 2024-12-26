3242ccde72e868f01afc3de08bbb39a7fd8dd3ce
Joni Vainio-Kaila <joni.vainio-kaila@vincit.fi>
2024-12-26 19:54:29 +0200

    fix(plugin): Do not run generators in a plugin unneccessarily

--- package/src/plugin/index.ts
+++ package/src/plugin/index.ts
@@ -1,28 +1,46 @@
 import type { NextConfig } from "next";
+import {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_EXPORT,
+  PHASE_PRODUCTION_BUILD,
+  PHASE_PRODUCTION_SERVER,
+  PHASE_TEST,
+} from "next/constants";
 import { spawn, spawnSync } from "node:child_process";
 import { addAliases } from "./addAliases";
 
+type Phase =
+  | typeof PHASE_EXPORT
+  | typeof PHASE_PRODUCTION_BUILD
+  | typeof PHASE_PRODUCTION_SERVER
+  | typeof PHASE_DEVELOPMENT_SERVER
+  | typeof PHASE_TEST;
+
 export function withNextGlobeGenPlugin(configPath = "./i18n.config.ts") {
-  if (!process.env.NEXT_PRIVATE_WORKER) useGenerator(configPath);
   return function withNextGlobeGen(config: NextConfig) {
-    addAliases(config, {
-      "next-globe-gen/schema": "./.next-globe-gen/schema.ts",
-      "next-globe-gen/messages": "./.next-globe-gen/messages.ts",
-    });
-    return config;
+    return (phase: Phase) => {
+      useGenerator(configPath, phase);
+      addAliases(config, {
+        "next-globe-gen/schema": "./.next-globe-gen/schema.ts",
+        "next-globe-gen/messages": "./.next-globe-gen/messages.ts",
+      });
+      return config;
+    };
   };
 }
 
-function useGenerator(configPath: string) {
+function useGenerator(configPath: string, phase: Phase) {
+  if (process.env.NEXT_PRIVATE_WORKER) return;
+  if (process.env.NEXT_DEPLOYMENT_ID !== undefined) return;
   try {
-    if (process.env.NODE_ENV === "development") {
+    if (phase === "phase-development-server") {
       spawn(`npx next-globe-gen --watch --config ${configPath}`, {
         cwd: process.cwd(),
         stdio: "inherit",
         shell: true,
         detached: false,
       });
-    } else {
+    } else if (phase !== "phase-production-server") {
       spawnSync(`npx next-globe-gen --config ${configPath}`, {
         cwd: process.cwd(),
         stdio: "inherit",
