fef99e7118087d90933883e177dc2aafb7479340
Joni Vainio-Kaila <joni.vainio-kaila@vincit.fi>
2024-12-26 22:06:03 +0200

    fix(docs): Fix colors of getting started section in light mode

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./{src,docs}/**/*.{js,jsx,ts,tsx,mdx}"],
   darkMode: ["selector", '[data-theme="dark"]'],
   corePlugins: {
     preflight: false,
