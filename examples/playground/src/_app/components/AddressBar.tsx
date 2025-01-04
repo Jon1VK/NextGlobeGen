52b5ef82762bda34190ebef9ec581321cfccd869
Joni Vainio-Kaila <joni.vainio-kaila@vincit.fi>
2025-01-04 09:54:38 +0200

    fix(playground): Fix address bar overflow on mobile view

--- examples/playground/src/_app/components/AddressBar.tsx
+++ examples/playground/src/_app/components/AddressBar.tsx
@@ -12,7 +12,7 @@ function LanguageSwitcher() {
   const route = useRoute();
 
   return (
-    <div className="ml-auto flex gap-x-2">
+    <div className="flex gap-x-2">
       {locales.map((locale) => (
         <Link
           key={locale}
@@ -67,53 +67,51 @@ export default function AddressBar() {
   const pathname = usePathname();
 
   return (
-    <div className="flex items-center gap-x-2 p-3.5 lg:px-5 lg:py-3">
-      <div className="text-gray-600">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-4"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </div>
-      <div className="flex gap-x-1 text-sm font-medium">
-        <div>
-          <span className="px-2 text-gray-400">acme.com</span>
+    <div className="flex text-nowrap items-center justify-between flex-wrap overflow-x-auto gap-4 p-3.5 lg:px-5 lg:py-3">
+      <div className="flex gap-x-2">
+        <div className="text-gray-600">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+              clipRule="evenodd"
+            />
+          </svg>
         </div>
-        {pathname ? (
-          <>
-            <span className="text-gray-600">/</span>
-            {pathname
-              .split("/")
-              .slice(1)
-              .map((segment) => {
-                return (
-                  <React.Fragment key={segment}>
-                    <span>
-                      <span
-                        key={segment}
-                        className="animate-[highlight_1s_ease-in-out_1] rounded-full px-1.5 py-0.5 text-gray-100"
-                      >
-                        {segment}
-                      </span>
+        <div className="flex gap-x-1 text-sm font-medium">
+          <div>
+            <span className="px-2 text-gray-400">acme.com</span>
+          </div>
+          <span className="text-gray-600">/</span>
+          {pathname
+            .split("/")
+            .slice(1)
+            .map((segment) => {
+              return (
+                <React.Fragment key={segment}>
+                  <span>
+                    <span
+                      key={segment}
+                      className="animate-[highlight_1s_ease-in-out_1] rounded-full px-1.5 py-0.5 text-gray-100"
+                    >
+                      {segment}
                     </span>
+                  </span>
 
-                    <span className="text-gray-600">/</span>
-                  </React.Fragment>
-                );
-              })}
-          </>
-        ) : null}
+                  <span className="text-gray-600">/</span>
+                </React.Fragment>
+              );
+            })}
 
-        <Suspense>
-          <Params />
-        </Suspense>
+          <Suspense>
+            <Params />
+          </Suspense>
+        </div>
       </div>
 
       <LanguageSwitcher />
