7bea02c049f5d1c8d8a6aab1608dd015cd9d5ddd
Joni Vainio-Kaila <joni.vainio-kaila@vincit.fi>
2025-01-08 13:24:08 +0200

    feat(package): Add backwards support for Next.js 14

--- package/src/router/shared/FormFactory.tsx
+++ package/src/router/shared/FormFactory.tsx
@@ -1,31 +1,31 @@
 import type { Locale, Route } from "next-globe-gen/schema";
-import { default as NextForm } from "next/form";
-import { type ComponentProps } from "react";
+import { forwardRef, type ComponentProps, type Ref } from "react";
 import {
   type HrefOptions,
   type ParamsOption,
   type useHrefFactory,
 } from "./useHrefFactory";
 
-type NextFormProps = ComponentProps<typeof NextForm>;
+type NextFormProps = ComponentProps<"form">;
 
 type FormProps<R extends Route> = Omit<NextFormProps, "action"> &
   ({
-    action: R | Exclude<NextFormProps["action"], string> | (string & {});
+    action:
+      | R
+      | Exclude<NextFormProps["action"], string | undefined>
+      | (string & {});
     locale?: Locale;
   } & ParamsOption<R>);
 
 export function FormFactory(useHref: ReturnType<typeof useHrefFactory>) {
-  return function Form<R extends Route>({
-    action,
-    locale,
-    params,
-    ...formProps
-  }: FormProps<R>) {
+  return forwardRef(function Form<R extends Route>(
+    { action, locale, params, ...formProps }: FormProps<R>,
+    ref?: Ref<HTMLFormElement>,
+  ) {
     if (typeof action === "function") {
-      return <NextForm {...formProps} action={action} />;
+      return <form {...formProps} action={action} />;
     }
     const options: HrefOptions<string> = { pathname: action, locale, params };
-    return <NextForm {...formProps} action={useHref(options)} />;
-  };
+    return <form {...formProps} ref={ref} action={useHref(options)} />;
+  }) as <R extends Route>(props: FormProps<R>) => JSX.Element;
 }
--- package/src/router/shared/LinkFactory.tsx
+++ package/src/router/shared/LinkFactory.tsx
@@ -1,6 +1,6 @@
 import type { Locale, Route } from "next-globe-gen/schema";
 import { default as NextLink } from "next/link";
-import { type ComponentProps } from "react";
+import { forwardRef, type ComponentProps, type Ref } from "react";
 import {
   extractHrefOptions,
   type HrefOptions,
@@ -18,20 +18,19 @@ type LinkProps<R extends Route> = Omit<NextLinkProps, "href"> &
   );
 
 export function LinkFactory(useHref: ReturnType<typeof useHrefFactory>) {
-  return function Link<R extends Route>({
-    href,
-    locale,
-    params,
-    ...linkProps
-  }: LinkProps<R>) {
+  return forwardRef(function Link<R extends Route>(
+    { href, locale, params, ...linkProps }: LinkProps<R>,
+    ref?: Ref<HTMLAnchorElement>,
+  ) {
     const useHrefArgs = [href, params ?? locale, locale] as UseHrefArgs<R>;
     const options = extractHrefOptions(useHrefArgs);
     return (
       <NextLink
         {...linkProps}
+        ref={ref}
         href={useHref(options)}
         hrefLang={options.locale}
       />
     );
-  };
+  }) as <R extends Route>(props: LinkProps<R>) => JSX.Element;
 }
