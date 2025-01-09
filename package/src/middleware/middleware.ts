98bbc2173f3f6ef8e3acb41331c258ab9eafe784
Joni Vainio-Kaila <joni.vainio-kaila@vincit.fi>
2025-01-09 14:39:17 +0200

    feat(package): Export middleware as a default export for convenience

--- package/src/middleware/middleware.ts
+++ package/src/middleware/middleware.ts
@@ -9,7 +9,25 @@ type MiddlewareOptions = {
   skipAlternateLinkHeader?: boolean;
 };
 
-export function middleware(request: NextRequest, opts?: MiddlewareOptions) {
+/**
+ * The middleware handles locale negotiation and adds alternate links of the page to the response headers.
+ *
+ * @example
+ * import nextGlobeGenMiddleware from "next-globe-gen/middleware";
+ * export function middleware(request) {
+ *   const response = nextGlobeGenMiddleware(request);
+ *   // other custom logic that possibly modify the response
+ *   return response;
+ * }
+ * // Ignore next internals and static assets
+ * export const config = {
+ *   matcher: ["/((?!_next|.*\\.).*)"],
+ * };
+ */
+export default function middleware(
+  request: NextRequest,
+  opts?: MiddlewareOptions,
+) {
   const pathname = request.nextUrl.pathname;
   const [locale_, routePathname] = extractLocaleAndRoutePathname(pathname);
 
@@ -68,3 +86,17 @@ function getAlternativeLinks(locale: Locale, request: NextRequest) {
     `, <${defaultURL}>; rel="alternate"; hreflang="x-default"`,
   );
 }
+
+/**
+ * The middleware handles locale negotiation and adds alternate links of the page to the response headers.
+ *
+ * @example
+ * export { middleware } from "next-globe-gen/middleware";
+ * // Ignore next internals and static assets
+ * export const config = {
+ *   matcher: ["/((?!_next|.*\\.).*)"],
+ * };
+ */
+const middlewareExport = middleware;
+
+export { middlewareExport as middleware };
