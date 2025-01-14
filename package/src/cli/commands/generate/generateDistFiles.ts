feb23ee3b31517e0a9fad57cceab7a3492195a7c
Joni Vainio-Kaila <joni.vainio-kaila@vincit.fi>
2025-01-14 17:50:44 +0200

    fix(package): Sort schema routes and prettify the generated schema and messages outputs

--- package/src/cli/commands/generate/generateDistFiles.ts
+++ package/src/cli/commands/generate/generateDistFiles.ts
@@ -8,6 +8,8 @@ import {
   isPageOriginRoute,
 } from "~/cli/utils/route-utils";
 import { toPascalCase } from "~/cli/utils/string-utils";
+import type { Messages } from "~/types/messages";
+import type { Schema } from "~/types/schema";
 import { getMessages } from "./getMessages";
 
 export const OUT_DIR = "./.next-globe-gen";
@@ -48,7 +50,15 @@ export function generateSchemaFile(
   config: Config,
   originRoutes: OriginRoute[],
 ) {
-  const routes: Record<string, Record<string, string>> = {};
+  const schema = generateSchema(config, originRoutes);
+  const JSONSchema = JSON.stringify(schema, null, "\t");
+  const schemaFile = template("schema").replace("{schema}", JSONSchema);
+  const schemaFilePath = path.join(OUT_DIR, "schema.ts");
+  writeFileSync(schemaFilePath, schemaFile);
+}
+
+function generateSchema(config: Config, originRoutes: OriginRoute[]) {
+  const routes: Schema["routes"] = {};
   originRoutes.forEach((originRoute) => {
     if (!isPageOriginRoute(originRoute)) return;
     const routeName = getRouteName(originRoute.path);
@@ -60,41 +70,66 @@ export function generateSchemaFile(
       },
     );
   });
-  const schema = {
+  return {
     locales: config.locales,
     defaultLocale: config.defaultLocale,
     prefixDefaultLocale: config.routes.prefixDefaultLocale,
-    routes,
-  };
-  const JSONSchema = JSON.stringify(schema);
-  const schemaFile = template("schema").replace("{schema}", JSONSchema);
-  const schemaFilePath = path.join(OUT_DIR, "schema.ts");
-  writeFileSync(schemaFilePath, schemaFile);
+    routes: sortedRoutes(routes),
+  } satisfies Schema;
+}
+
+function sortedRoutes(routes: Schema["routes"]) {
+  const dynamicSegmentRegexp = /\[\.{0,3}/;
+  const catchAllSegmentRegexp = /\[\.{3}/;
+  return Object.fromEntries(
+    Object.entries(routes).sort(([a], [b]) => {
+      const aIsDynamic = dynamicSegmentRegexp.test(a);
+      const bIsDynamic = dynamicSegmentRegexp.test(b);
+      if (!aIsDynamic && !bIsDynamic) return a.localeCompare(b);
+      if (!bIsDynamic) return 1;
+      if (!aIsDynamic) return -1;
+      const aIsCatchAll = catchAllSegmentRegexp.test(a);
+      const bIsCatchAll = catchAllSegmentRegexp.test(b);
+      if (aIsCatchAll && !bIsCatchAll) return 1;
+      if (!aIsCatchAll && bIsCatchAll) return -1;
+      const dynamicDepthA = a.split("[").at(0)!.match(/\//g)!.length;
+      const dynamicDepthB = b.split("[").at(0)!.match(/\//g)!.length;
+      const dynamicDepthDiff = dynamicDepthB - dynamicDepthA;
+      if (dynamicDepthDiff !== 0) return dynamicDepthDiff;
+      return a.localeCompare(b);
+    }),
+  );
 }
 
 export async function generateMessagesFile(config: Config) {
   const messages = await getMessages(config);
-  const JSONMessages = JSON.stringify(messages);
-  const clientKeys =
-    config.messages.clientKeys instanceof RegExp
-      ? [config.messages.clientKeys]
-      : config.messages.clientKeys;
-  const clientMessages = !clientKeys
-    ? messages
-    : Object.fromEntries(
-        Object.entries(messages).map(([locale, localeMessages]) => {
-          const filteredMessages = Object.fromEntries(
-            Object.entries(localeMessages).filter(([key]) => {
-              return clientKeys.some((regExp) => regExp.test(key));
-            }),
-          );
-          return [locale, filteredMessages];
-        }),
-      );
-  const JSONClientMessages = JSON.stringify(clientMessages);
+  const JSONMessages = JSON.stringify(messages, null, "\t");
+  const clientMessages = getClientMessages(config, messages);
+  const JSONClientMessages = clientMessages
+    ? JSON.stringify(clientMessages, null, "\t")
+    : "messages";
   const messagesFile = template("messages")
     .replace("{messages}", JSONMessages)
     .replace("{clientMessages}", JSONClientMessages);
   const messagesFilePath = path.join(OUT_DIR, "messages.ts");
   writeFileSync(messagesFilePath, messagesFile);
+}
+
+function getClientMessages(config: Config, messages: Messages) {
+  const clientKeys =
+    config.messages.clientKeys instanceof RegExp
+      ? [config.messages.clientKeys]
+      : config.messages.clientKeys;
+  if (!clientKeys) return undefined;
+  const clientMessages = Object.fromEntries(
+    Object.entries(messages).map(([locale, localeMessages]) => {
+      const filteredMessages = Object.fromEntries(
+        Object.entries(localeMessages).filter(([key]) => {
+          return clientKeys.some((regExp) => regExp.test(key));
+        }),
+      );
+      return [locale, filteredMessages];
+    }),
+  );
+  return clientMessages;
 }
