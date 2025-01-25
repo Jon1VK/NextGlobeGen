fc771d5642badaeea14d989802ae54f61656cd2f
Joni Vainio-Kaila <joni.vainio-kaila@vincit.fi>
2025-01-25 14:33:17 +0200

    chore(playground): Configure vitest unit testing

---
+++ examples/playground/src/test-utils.tsx
@@ -0,0 +1,25 @@
+import { render, type RenderOptions } from "@testing-library/react";
+import { IntlProvider } from "next-globe-gen";
+import type { ReactNode } from "react";
+import { messages } from "../.next-globe-gen/messages";
+import { schema } from "../.next-globe-gen/schema";
+
+const wrapper = ({ children }: { children: ReactNode }) => (
+  <IntlProvider
+    schema={schema}
+    messages={messages[schema.defaultLocale]}
+    locale={schema.defaultLocale}
+  >
+    {children}
+  </IntlProvider>
+);
+
+const customRender = ((ui: ReactNode, options?: RenderOptions) => {
+  return render(ui, { wrapper, ...options });
+}) as typeof render;
+
+// re-export everything
+export * from "@testing-library/react";
+
+// override render method
+export { customRender as render };
