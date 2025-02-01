562e20779342b3645ccb1d2174766e16b26e8f84
Joni Vainio-Kaila <joni.vainio-kaila@vincit.fi>
2025-02-01 20:24:23 +0200

    fix(package): Avoid user files esm bundling issues by bundling to cjs

--- package/src/utils/ts-utils.ts
+++ package/src/utils/ts-utils.ts
@@ -1,21 +1,11 @@
 import { createHash } from "crypto";
 import { rmSync } from "fs";
 import path from "path";
-import { build, type Options } from "tsup";
+import { build } from "tsup";
 import { pathToFileURL } from "url";
 import { isFile } from "~/utils/fs-utils";
 
 const OUT_DIR = "./.next-globe-gen";
-
-type Plugin = Options["plugins"] extends (infer T)[] | undefined ? T : never;
-
-const jsImportsPlugin: Plugin = {
-  name: "js-imports-plugin",
-  renderChunk(_, chunk) {
-    const code = chunk.code.replace(/from "(.*)"/g, 'from "$1.js"');
-    return { code };
-  },
-};
 
 export async function compile<T>(filePath: string) {
   const version = new Date().getTime();
@@ -25,22 +15,21 @@ export async function compile<T>(filePath: string) {
   await build({
     config: false,
     outDir: OUT_DIR,
-    format: "esm",
+    format: "cjs",
     bundle: true,
     splitting: false,
     target: "node18",
-    plugins: [jsImportsPlugin],
     entryPoints: { [`${outputFileName}`]: filePath },
     silent: true,
   });
   const compiledPath = path.resolve(OUT_DIR, outputFileName);
   const compiledFileURL = pathToFileURL(compiledPath);
-  if (isFile(`${compiledPath}.mjs`)) {
-    const contents = await import(`${compiledFileURL}.mjs`);
-    rmSync(`${compiledPath}.mjs`);
-    return contents as T;
+  if (isFile(`${compiledPath}.cjs`)) {
+    const contents = await import(`${compiledFileURL}.cjs`);
+    rmSync(`${compiledPath}.cjs`);
+    return contents.default as T;
   }
   const contents = await import(`${compiledFileURL}.js`);
   rmSync(`${compiledPath}.js`);
-  return contents as T;
+  return contents.default as T;
 }
