9a02c712bdb4be2e32881903200f87e773086534
Joni Vainio-Kaila <joni.vainio-kaila@vincit.fi>
2025-11-26 19:22:20 +0200

    fix(package)/#237: Fix rendering of messages with dot separator in a key
    Closes #237

--- package/src/cli/commands/generate/getMessages.ts
+++ package/src/cli/commands/generate/getMessages.ts
@@ -1,12 +1,13 @@
-import type { Messages } from "~/types/messages";
+import { flatten, unflatten } from "~/cli/utils/obj-utils";
 import { getLocales, type Config } from "~/utils/config";
 
 export async function getMessages(config: Config) {
-  const messages: Messages = {};
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const messages: Record<string, any> = {};
   const locales = getLocales(config);
   for await (const locale of locales) {
     const localeMessages = await config.messages.getMessages(locale);
-    messages[locale] = localeMessages;
+    messages[locale] = unflatten(flatten(localeMessages));
   }
   return messages;
 }
