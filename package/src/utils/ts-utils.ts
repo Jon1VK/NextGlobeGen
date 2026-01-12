60021e6375dfec17086184a4e21491bab01baddb
Joni Vainio-Kaila <joni.vainio-kaila@vincit.fi>
2026-01-12 11:51:43 +0200

    fix(package): Handle ts compilation correctly

--- package/src/utils/ts-utils.ts
+++ package/src/utils/ts-utils.ts
@@ -5,31 +5,36 @@ import { build } from "tsup";
 import { pathToFileURL } from "url";
 import { isFile } from "~/utils/fs-utils";
 
-const OUT_DIR = "./next-globe-gen";
-
 export async function compile<T>(filePath: string) {
+  const outDir = path.dirname(filePath);
   const version = Math.random();
   const outputFileName = createHash("md5")
     .update(`${filePath}-${version}`)
     .digest("hex");
   await build({
     config: false,
-    outDir: OUT_DIR,
+    outDir,
     format: "cjs",
     bundle: true,
     splitting: false,
     target: "node18",
     entryPoints: { [`${outputFileName}`]: filePath },
     silent: true,
+    shims: true,
   });
-  const compiledPath = path.resolve(OUT_DIR, outputFileName);
-  const compiledFileURL = pathToFileURL(compiledPath);
+  const compiledPath = path.resolve(outDir, outputFileName);
   if (isFile(`${compiledPath}.cjs`)) {
-    const contents = await import(`${compiledFileURL}.cjs`);
-    rmSync(`${compiledPath}.cjs`);
-    return contents.default as T;
+    return importContents<T>(`${compiledPath}.cjs`);
   }
-  const contents = await import(`${compiledFileURL}.js`);
-  rmSync(`${compiledPath}.js`);
-  return contents.default as T;
+  return importContents<T>(`${compiledPath}.js`);
+}
+
+async function importContents<T>(filePath: string): Promise<T> {
+  try {
+    const fileURL = pathToFileURL(filePath);
+    const contents = await import(fileURL.href);
+    return contents.default as T;
+  } finally {
+    rmSync(filePath);
+  }
 }
