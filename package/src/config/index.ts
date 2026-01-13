6dc36dd2a1f9ff92201bd64be00fe1d244e04440
Joni Vainio-Kaila <joni.vainio-kaila@vincit.fi>
2026-01-13 20:11:24 +0200

    fix(package): Write extracted message keys properly to nested dirs

--- package/src/config/index.ts
+++ package/src/config/index.ts
@@ -1,4 +1,4 @@
 export { DEFAULT_CONFIG } from "./config";
 export * from "./types";
-export type { UserConfig as Config } from "./types";
+export type { UserConfig as Config, Config as ResolvedConfig } from "./types";
 export * from "./utils";
